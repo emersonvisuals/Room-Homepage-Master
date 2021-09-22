93ec0061244aae6bc581c231ce40c7f4f69ecb2b
Thomas Emerson <emersonvisuals@gmail.com>
2021-09-22 19:59:24 +1000

    wip

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
- 
+const header = document.querySelector('.wrapper h1');
+const para = document.querySelector('.wrapper p');
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+
+right.addEventListener('click', function() {
+    console.log('clicked');
+    header.innerHTML="hello";
+});
