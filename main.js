5d14acdcfc718cae15ca4c5f5322e7850babff66
Thomas Emerson <emersonvisuals@gmail.com>
2021-09-26 19:48:43 +1000

    mobile nav wip

--- main.js
+++ main.js
@@ -66,59 +66,19 @@ left.addEventListener('click', function(){
 
     header.classList.toggle('active');
     paragraph.classList.toggle('active');
-})
+});
 
 
 
 
+// mobile burger 
 
+const toggle = document.querySelector('#toggle');
+const dropdown = document.querySelector('#dropdown');
 
-/*
-const object = [
-    {
-        heading:"We are available all across the globe",
-        text:"With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today."
-    },
-
-    {
-        heading:"Manufactured with the best materials",
-        text:"Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office."
-    },
-
-    {
-        heading:"Discover innovative ways to decorate",
-        text:"We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love."
-    }
-];
-
-let currentItem = 0; 
-
-
-
-console.log("start");
-
-right.addEventListener('click',function(){
-    let item = object[currentItem];
-    currentItem++;
-    if(currentItem > object.length-1){
-currentItem = 0;
-    }
-    header.textContent = item.heading;
-    paragraph.textContent = item.text;
-    console.log(`i am value no ${currentItem}`)
-})
-
-/*
-left.addEventListener('click',function(){
-    let item = object[currentItem];
-    currentItem--;
-    if(currentItem < 0){
-currentItem = 2;
-    }
-    header.textContent = item.heading;
-    para.textContent = item.text;
-    console.log(`i am value no ${currentItem}`)
-})
-*/
+toggle.addEventListener('click', function(){
+    toggle.classList.toggle('active');
+    dropdown.classList.toggle('active');
+});
 
 
